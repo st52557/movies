4c4c18e5dd64ad683214af3e4ff38f3e5627df65
Lukas Semorad <st52557@student.upce.cz>
2021-03-22 08:29:15 +0100

    crud

--- src/main/java/com/example/movies/repository/FilmRepository.java
+++ src/main/java/com/example/movies/repository/FilmRepository.java
@@ -14,6 +14,8 @@ public interface FilmRepository extends JpaRepository<Film, Long> {
 
     void deleteFilmByTitle(String string);
 
+    List<Film>findAll();
+
     List<Film>findFilmsByTitleContaining(String string);
 
     @Query(" select p from Film p where p.id between :from and :to")
